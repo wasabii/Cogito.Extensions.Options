78e1837db65601ef55d29ac0fc57be29d97712f7
Jerome Haltom <wasabi@cogito.cx>
2019-04-12 20:54:15 -0500

    Initial commit.

---
+++ Cogito.Extensions.Options/OptionsConfigurationServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Cogito.Extensions.Options
+{
+
+    public static class OptionsConfigurationServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name) where TOptions : class
+            => services.Configure<TOptions>(name, _ => { });
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <param name="configureBinder">Used to configure the <see cref="BinderOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, Action<BinderOptions> configureBinder)
+            where TOptions : class
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.AddSingleton<IOptionsChangeTokenSource<TOptions>>(a => new ConfigurationChangeTokenSource<TOptions>(name, a.GetRequiredService<IConfigurationRoot>()));
+            return services.AddSingleton<IConfigureOptions<TOptions>>(a => new NamedConfigureFromConfigurationOptions<TOptions>(name, a.GetRequiredService<IConfigurationRoot>(), configureBinder));
+        }
+
+    }
+
+}
