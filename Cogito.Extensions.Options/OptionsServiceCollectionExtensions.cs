98becf8f3d9bb24a50ab23a24ef69c1a1d3accba
Jerome Haltom <wasabi@cogito.cx>
2019-06-27 00:33:13 -0500

    Added post configure method.

--- Cogito.Extensions.Options/OptionsServiceCollectionExtensions.cs
+++ Cogito.Extensions.Options/OptionsServiceCollectionExtensions.cs
@@ -43,6 +43,40 @@ namespace Cogito.Extensions.Options
             return Configure<TOptions>(services, Microsoft.Extensions.Options.Options.DefaultName, configure);
         }
 
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection PostConfigure<TOptions>(this IServiceCollection services, string name, Action<IServiceProvider, TOptions> configure)
+            where TOptions : class
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure));
+
+            services.AddOptions();
+            services.AddSingleton(p => (IPostConfigureOptions<TOptions>)new PostConfigureOptions<TOptions>(name, o => configure(p, o)));
+            return services;
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection PostConfigure<TOptions>(this IServiceCollection services, Action<IServiceProvider, TOptions> configure)
+            where TOptions : class
+        {
+            return PostConfigure<TOptions>(services, Microsoft.Extensions.Options.Options.DefaultName, configure);
+        }
+
     }
 
 }
