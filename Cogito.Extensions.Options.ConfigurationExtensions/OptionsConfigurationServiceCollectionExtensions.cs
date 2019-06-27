b8a3ab62233caa4326fb923b220ab0484119f8e3
Jerome Haltom <wasabi@cogito.cx>
2019-06-27 00:18:44 -0500

    Added some methods to configure options with IServiceProvider access.

--- Cogito.Extensions.Options.ConfigurationExtensions/OptionsConfigurationServiceCollectionExtensions.cs
+++ Cogito.Extensions.Options.ConfigurationExtensions/OptionsConfigurationServiceCollectionExtensions.cs
@@ -16,6 +16,25 @@ namespace Cogito.Extensions.Options
         /// <typeparam name="TOptions">The type of options being configured.</typeparam>
         /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
         /// <param name="name">The name of the options instance.</param>
+        /// <param name="configureBinder">Used to configure the <see cref="BinderOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, Action<BinderOptions> configureBinder)
+            where TOptions : class
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.AddSingleton<IOptionsChangeTokenSource<TOptions>>(a => new ConfigurationChangeTokenSource<TOptions>(name, a.GetRequiredService<IConfigurationRoot>()));
+            return services.AddSingleton<IConfigureOptions<TOptions>>(a => new NamedConfigureFromConfigurationOptions<TOptions>(name, a.GetRequiredService<IConfigurationRoot>(), configureBinder));
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name)
             where TOptions : class
@@ -34,25 +53,6 @@ namespace Cogito.Extensions.Options
             where TOptions : class
         {
             return Configure<TOptions>(services, Microsoft.Extensions.Options.Options.DefaultName, configureBinder);
-        }
-
-        /// <summary>
-        /// Registers a configuration instance which TOptions will bind against.
-        /// </summary>
-        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
-        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
-        /// <param name="name">The name of the options instance.</param>
-        /// <param name="configureBinder">Used to configure the <see cref="BinderOptions"/>.</param>
-        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, Action<BinderOptions> configureBinder)
-            where TOptions : class
-        {
-            if (services == null)
-                throw new ArgumentNullException(nameof(services));
-
-            services.AddOptions();
-            services.AddSingleton<IOptionsChangeTokenSource<TOptions>>(a => new ConfigurationChangeTokenSource<TOptions>(name, a.GetRequiredService<IConfigurationRoot>()));
-            return services.AddSingleton<IConfigureOptions<TOptions>>(a => new NamedConfigureFromConfigurationOptions<TOptions>(name, a.GetRequiredService<IConfigurationRoot>(), configureBinder));
         }
 
     }
---
+++ Cogito.Extensions.Options/OptionsServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Cogito.Extensions.Options
+{
+
+    public static class OptionsServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, Action<IServiceProvider, TOptions> configure)
+            where TOptions : class
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.AddSingleton(p => (IConfigureOptions<TOptions>)new ConfigureNamedOptions<TOptions>(name, o => configure(p, o)));
+            return services;
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, Action<IServiceProvider, TOptions> configure)
+            where TOptions : class
+        {
+            return Configure<TOptions>(services, Microsoft.Extensions.Options.Options.DefaultName, configure);
+        }
+
+    }
+
+}
