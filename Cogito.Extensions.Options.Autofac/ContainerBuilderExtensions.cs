a4eb9214d1c61cf3457ee3147cc3aef5e3bdd5de
Jerome Haltom <jhaltom@fileandserve.com>
2019-09-09 23:42:10 -0500

    Add some overloads that take Action overrides.

--- Cogito.Extensions.Options.Autofac/ContainerBuilderExtensions.cs
+++ Cogito.Extensions.Options.Autofac/ContainerBuilderExtensions.cs
@@ -5,6 +5,7 @@ using Autofac;
 using Cogito.Autofac.DependencyInjection;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Cogito.Extensions.Options.Autofac
 {
@@ -20,7 +21,26 @@ namespace Cogito.Extensions.Options.Autofac
         /// <param name="name">The name of the options instance.</param>
         /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
         /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
-        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, string name, Action<IServiceProvider, TOptions> configure)
+        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, string name, Action<IComponentContext, TOptions> configure)
+            where TOptions : class, new()
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure));
+
+            return builder.Populate(s => s.Configure<TOptions>(name, (c, o) => configure(c.GetRequiredService<IComponentContext>(), o)));
+        }
+
+        /// <summary>
+        /// Registers a type provided by the <see cref="IConfigurationRoot"/> at the given path.
+        /// </summary>
+        /// <typeparam name="TOptions"></typeparam>
+        /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
+        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, string name, Action<TOptions> configure)
             where TOptions : class, new()
         {
             if (builder == null)
@@ -38,7 +58,25 @@ namespace Cogito.Extensions.Options.Autofac
         /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
         /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
         /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
-        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, Action<IServiceProvider, TOptions> configure)
+        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, Action<IComponentContext, TOptions> configure)
+            where TOptions : class, new()
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure));
+
+            return builder.Populate(s => s.Configure<TOptions>((c, o) => configure(c.GetRequiredService<IComponentContext>(), o)));
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
+        public static ContainerBuilder Configure<TOptions>(this ContainerBuilder builder, Action<TOptions> configure)
             where TOptions : class, new()
         {
             if (builder == null)
@@ -57,7 +95,26 @@ namespace Cogito.Extensions.Options.Autofac
         /// <param name="name">The name of the options instance.</param>
         /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
         /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
-        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, string name, Action<IServiceProvider, TOptions> configure)
+        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, string name, Action<IComponentContext, TOptions> configure)
+            where TOptions : class
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure));
+
+            return builder.Populate(s => s.PostConfigure<TOptions>(name, (c, o) => configure(c.GetRequiredService<IComponentContext>(), o)));
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
+        /// <param name="name">The name of the options instance.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
+        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, string name, Action<TOptions> configure)
             where TOptions : class
         {
             if (builder == null)
@@ -75,7 +132,20 @@ namespace Cogito.Extensions.Options.Autofac
         /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
         /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
         /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
-        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, Action<IServiceProvider, TOptions> configure)
+        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, Action<IComponentContext, TOptions> configure)
+            where TOptions : class
+        {
+            return builder.Populate(s => s.PostConfigure<TOptions>((c, o) => configure(c.GetRequiredService<IComponentContext>(), o)));
+        }
+
+        /// <summary>
+        /// Registers a configuration instance which TOptions will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="builder">The <see cref="ContainerBuilder"/> to add the services to.</param>
+        /// <param name="configure">Used to configure the <typeparamref name="TOptions"/>.</param>
+        /// <returns>The <see cref="ContainerBuilder"/> so that additional calls can be chained.</returns>
+        public static ContainerBuilder PostConfigure<TOptions>(this ContainerBuilder builder, Action<TOptions> configure)
             where TOptions : class
         {
             return builder.Populate(s => s.PostConfigure<TOptions>(configure));
