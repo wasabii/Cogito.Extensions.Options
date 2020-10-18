6d0394b2fa9b34b26e0b6fd900fec009e059d7fc
Jerome Haltom <jhaltom@alethic.solutions>
2020-10-18 15:15:40 -0500

    Update to latest dependent packages, which depend on Autofac 6.

--- Cogito.Extensions.Options.Autofac/ContainerBuilderExtensions.cs
+++ Cogito.Extensions.Options.Autofac/ContainerBuilderExtensions.cs
@@ -4,7 +4,6 @@ using Autofac;
 
 using Cogito.Autofac.DependencyInjection;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cogito.Extensions.Options.Autofac
--- Cogito.Extensions.Options.Configuration/OptionsConfigurationServiceCollectionExtensions.cs
+++ Cogito.Extensions.Options.Configuration/OptionsConfigurationServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
-namespace Cogito.Extensions.Options
+namespace Cogito.Extensions.Options.Configuration
 {
 
     public static class OptionsConfigurationServiceCollectionExtensions
